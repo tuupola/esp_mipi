1c9776055ea8783df59baef4f93455b4c05b9368
Mika Tuupola <tuupola@appelsiini.net>
2019-09-20 21:49:05 +0300

    Improve readability

--- st7735s.c
+++ st7735s.c
@@ -41,43 +41,42 @@ SOFTWARE.
 #include <esp_log.h>
 
 #include "sdkconfig.h"
-#include "ili9341.h"
+#include "st7735s.h"
 
-static const char *TAG = "ili9341";
+static const char *TAG = "st7735s";
 static const uint8_t DELAY_BIT = 1 << 7;
 
 static SemaphoreHandle_t mutex;
 
 DRAM_ATTR static const lcd_init_cmd_t st_init_commands[]={
-    {0x01, {0}, 0 | DELAY_BIT},
-    {0x11, {0}, 0 | DELAY_BIT},
-
-    {0xB1, {0x01, 0x2C, 0x2D}, 3}, // Frame Rate Control
-    {0xB2, {0x01, 0x2C, 0x2D}, 3}, // Frame Rate Control
-    {0xB3, {0x01, 0x2C, 0x2D, 0x01, 0x2C, 0x2D}, 6}, // Frame Rate Control
-    {0xB4, {0x07}, 1}, // Display Inversion Control
-    {0xC0, {0xA2, 0x02, 0x84}, 3}, // Power Control 1
-    {0xC1, {0xC5}, 1}, // Power Control 2
-    {0xC2, {0x0A, 0x00}, 2}, // Power Control 3
-    {0xC3, {0x8A, 0x2A}, 2}, // Power Control 4
-    {0xC4, {0x8A, 0xEE}, 2}, // Power Control 5
-    {0xC5, {0x0E}, 1}, // VCOM Control 1
-    {0x20, {0}, 0}, // Display Inversion Off
-    {0x36, {0xC0}, 1}, // Memory Data Access Control 0xC8
-    {0x3A, {0x05}, 1}, // Interface Pixel Format
-    {0x2A, {0x00, 0x02, 0x00, 0x81}, 4}, // Column Address Set
-    {0x2B, {0x00, 0x01, 0x00, 0xA0}, 4}, // Row Address Set
-    {0x21, {0}, 0}, // Display Inversion On
-    {0xE0, {0x02, 0x1C, 0x07, 0x12, 0x37, 0x32, 0x29, 0x2D, 0x29, 0x25, 0x2B, 0x39, 0x00, 0x01, 0x03, 0x10}, 16}, // Gamma (‘+’polarity) Correction Characteristics Setting
-    {0xE1, {0x03, 0x1D, 0x07, 0x06, 0x2E, 0x2C, 0x29, 0x2D, 0x2E, 0x2E, 0x37, 0x3F, 0x00, 0x00, 0x02, 0x10}, 16}, // Gamma ‘-’polarity Correction Characteristics Setting
-    {0x13, {0}, 0 | DELAY_BIT}, // Normal Display Mode On
-    {0x29, {0}, 0 | DELAY_BIT}, // Display On
+    {ST7735S_SWRESET, {0}, 0 | DELAY_BIT},
+    {ST7735S_SLPOUT, {0}, 0 | DELAY_BIT},
+    {ST7735S_FRMCTR1, {0x01, 0x2C, 0x2D}, 3},
+    {ST7735S_FRMCTR2, {0x01, 0x2C, 0x2D}, 3},
+    {ST7735S_FRMCTR3, {0x01, 0x2C, 0x2D, 0x01, 0x2C, 0x2D}, 6},
+    {ST7735S_INVCTR, {0x07}, 1},
+    {ST7735S_PWCTR1, {0xA2, 0x02, 0x84}, 3},
+    {ST7735S_PWCTR2, {0xC5}, 1},
+    {ST7735S_PWCTR3, {0x0A, 0x00}, 2},
+    {ST7735S_PWCTR4, {0x8A, 0x2A}, 2},
+    {ST7735S_PWCTR5, {0x8A, 0xEE}, 2},
+    {ST7735S_VMCTR1, {0x0E}, 1},
+    {ST7735S_INVOFF, {0}, 0},
+    {ST7735S_MADCTL, {0xC0}, 1},
+    {ST7735S_COLMOD, {0x05}, 1},
+    {ST7735S_CASET, {0x00, 0x02, 0x00, 0x81}, 4},
+    {ST7735S_RASET, {0x00, 0x01, 0x00, 0xA0}, 4},
+    {ST7735S_INVON, {0}, 0},
+    {ST7735S_GMCTRP1, {0x02, 0x1C, 0x07, 0x12, 0x37, 0x32, 0x29, 0x2D, 0x29, 0x25, 0x2B, 0x39, 0x00, 0x01, 0x03, 0x10}, 16}, // Gamma (‘+’polarity) Correction Characteristics Setting
+    {ST7735S_GMCTRN1, {0x03, 0x1D, 0x07, 0x06, 0x2E, 0x2C, 0x29, 0x2D, 0x2E, 0x2E, 0x37, 0x3F, 0x00, 0x00, 0x02, 0x10}, 16}, // Gamma ‘-’polarity Correction Characteristics Setting
+    {ST7735S_NORON, {0}, 0 | DELAY_BIT},
+    {ST7735S_DISPON, {0}, 0 | DELAY_BIT},
     /* End of commands . */
     {0, {0}, 0xff},
 };
 
 /* Uses spi_device_transmit, which waits until the transfer is complete. */
-static void ili9341_command(spi_device_handle_t spi, const uint8_t command)
+static void st7735s_command(spi_device_handle_t spi, const uint8_t command)
 {
     spi_transaction_t transaction;
 
@@ -89,7 +88,7 @@ static void ili9341_command(spi_device_handle_t spi, const uint8_t command)
 }
 
 /* Uses spi_device_transmit, which waits until the transfer is complete. */
-static void ili9341_data(spi_device_handle_t spi, const uint8_t *data, uint16_t length)
+static void st7735s_data(spi_device_handle_t spi, const uint8_t *data, uint16_t length)
 {
     spi_transaction_t transaction;
 
@@ -104,13 +103,13 @@ static void ili9341_data(spi_device_handle_t spi, const uint8_t *data, uint16_t 
 
 /* This function is called (in irq context!) just before a transmission starts. */
 /* It will set the DC line to the value indicated in the user field. */
-static void ili9341_pre_callback(spi_transaction_t *transaction)
+static void st7735s_pre_callback(spi_transaction_t *transaction)
 {
     int dc=(int)transaction->user;
-    gpio_set_level(CONFIG_ILI9341_PIN_DC, dc);
+    gpio_set_level(CONFIG_ST7735S_PIN_DC, dc);
 }
 
-static void ili9341_wait(spi_device_handle_t spi)
+static void st7735s_wait(spi_device_handle_t spi)
 {
     spi_transaction_t *rtrans;
 
@@ -121,12 +120,12 @@ static void ili9341_wait(spi_device_handle_t spi)
     }
 }
 
-static void ili9431_spi_master_init(spi_device_handle_t *spi)
+static void st7735s_spi_master_init(spi_device_handle_t *spi)
 {
     spi_bus_config_t buscfg = {
-        .miso_io_num = CONFIG_ILI9341_PIN_MISO,
-        .mosi_io_num = CONFIG_ILI9341_PIN_MOSI,
-        .sclk_io_num = CONFIG_ILI9341_PIN_CLK,
+        .miso_io_num = CONFIG_ST7735S_PIN_MISO,
+        .mosi_io_num = CONFIG_ST7735S_PIN_MOSI,
+        .sclk_io_num = CONFIG_ST7735S_PIN_CLK,
         .quadwp_io_num = -1,
         .quadhd_io_num = -1,
         /* Max transfer size in bytes */
@@ -135,53 +134,53 @@ static void ili9431_spi_master_init(spi_device_handle_t *spi)
     spi_device_interface_config_t devcfg = {
         .clock_speed_hz = CONFIG_SPI_CLOCK_SPEED_HZ,
         .mode = 0,
-        .spics_io_num = CONFIG_ILI9341_PIN_CS,
+        .spics_io_num = CONFIG_ST7735S_PIN_CS,
         .queue_size = 64,
         /* Handles the D/C line */
-        .pre_cb = ili9341_pre_callback,
+        .pre_cb = st7735s_pre_callback,
         .flags = SPI_DEVICE_NO_DUMMY
     };
     ESP_ERROR_CHECK(spi_bus_initialize(HSPI_HOST, &buscfg, 1));
     ESP_ERROR_CHECK(spi_bus_add_device(HSPI_HOST, &devcfg, spi));
 }
 
-void ili9341_init(spi_device_handle_t *spi)
+void st7735s_init(spi_device_handle_t *spi)
 {
     uint8_t cmd = 0;
 
     mutex = xSemaphoreCreateMutex();
 
     /* Init spi driver. */
-    ili9431_spi_master_init(spi);
+    st7735s_spi_master_init(spi);
 
     /* Init non spi gpio. */
-    gpio_set_direction(CONFIG_ILI9341_PIN_DC, GPIO_MODE_OUTPUT);
-    gpio_set_direction(CONFIG_ILI9341_PIN_RST, GPIO_MODE_OUTPUT);
-    gpio_set_direction(CONFIG_ILI9341_PIN_BCKL, GPIO_MODE_OUTPUT);
+    gpio_set_direction(CONFIG_ST7735S_PIN_DC, GPIO_MODE_OUTPUT);
+    gpio_set_direction(CONFIG_ST7735S_PIN_RST, GPIO_MODE_OUTPUT);
+    gpio_set_direction(CONFIG_ST7735S_PIN_BCKL, GPIO_MODE_OUTPUT);
 
     /* Reset the display. */
-    gpio_set_level(CONFIG_ILI9341_PIN_RST, 0);
+    gpio_set_level(CONFIG_ST7735S_PIN_RST, 0);
     vTaskDelay(100 / portTICK_RATE_MS);
-    gpio_set_level(CONFIG_ILI9341_PIN_RST, 1);
+    gpio_set_level(CONFIG_ST7735S_PIN_RST, 1);
     vTaskDelay(100 / portTICK_RATE_MS);
 
     ESP_LOGI(TAG, "Initialize the display.");
 
     /* Send all the commands. */
-    whilest[cmd].bytes != 0xff) {
-        ili9341_command(*spist[cmd].cmd);
-        ili9341_data(*spist[cmd].datast[cmd].bytes & 0x1F);
-        ifst[cmd].bytes & DELAY_BIT) {
+    while (st_init_commands[cmd].bytes != 0xff) {
+        st7735s_command(*spi, st_init_commands[cmd].cmd);
+        st7735s_data(*spi, st_init_commands[cmd].data, st_init_commands[cmd].bytes & 0x1F);
+        if (st_init_commands[cmd].bytes & DELAY_BIT) {
             vTaskDelay(100 / portTICK_RATE_MS);
         }
         cmd++;
     }
 
     /* Enable backlight. */
-    gpio_set_level(CONFIG_ILI9341_PIN_BCKL, 1);
+    gpio_set_level(CONFIG_ST7735S_PIN_BCKL, 1);
 }
 
-void ili9431_blit(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t *bitmap)
+void st7735s_blit(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t *bitmap)
 {
     if (0 == w || 0 == h) {
         return;
@@ -215,19 +214,19 @@ void ili9431_blit(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t w,
     }
 
     /* Column Address Set */
-    trans[0].tx_data[0] = ILI9341_CASET;
+    trans[0].tx_data[0] = ST7735S_CASET;
     trans[1].tx_data[0] = x1 >> 8;
     trans[1].tx_data[1] = x1 & 0xff;
     trans[1].tx_data[2] = x2 >> 8;
     trans[1].tx_data[3] = x2 & 0xff;
     /* Page Address Set */
-    trans[2].tx_data[0] = ILI9341_PASET;
+    trans[2].tx_data[0] = ST7735S_RASET;
     trans[3].tx_data[0] = y1 >> 8;
     trans[3].tx_data[1] = y1 & 0xff;
     trans[3].tx_data[2] = y2 >> 8;
     trans[3].tx_data[3] = y2 & 0xff;
     /* Memory Write */
-    trans[4].tx_data[0] = ILI9341_RAMWR;
+    trans[4].tx_data[0] = ST7735S_RAMWR;
     trans[5].tx_buffer = bitmap;
     /* Transfer size in bits */
     trans[5].length = size * DISPLAY_DEPTH;
@@ -238,12 +237,12 @@ void ili9431_blit(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t w,
         ESP_ERROR_CHECK(spi_device_queue_trans(spi, &trans[x], portMAX_DELAY));
     }
     /* Could do stuff here... */
-    ili9341_wait(spi);
+    st7735s_wait(spi);
 
     xSemaphoreGive(mutex);
 }
 
-void ili9431_putpixel(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t colour)
+void st7735s_putpixel(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t colour)
 {
-    ili9431_blit(spi, x1, y1, 1, 1, &colour);
+    st7735s_blit(spi, x1, y1, 1, 1, &colour);
 }
--- st7735s.h
+++ st7735s.h
@@ -31,8 +31,8 @@ SOFTWARE.
 
 #include "sdkconfig.h"
 
-#define DISPLAY_WIDTH   (CONFIG_ILI9341_DISPLAY_WIDTH)
-#define DISPLAY_HEIGHT  (CONFIG_ILI9341_DISPLAY_HEIGHT)
+#define DISPLAY_WIDTH   (CONFIG_ST7735S_DISPLAY_WIDTH)
+#define DISPLAY_HEIGHT  (CONFIG_ST7735S_DISPLAY_HEIGHT)
 #define DISPLAY_DEPTH   (16)
 
 #define SPI_MAX_TRANSFER_SIZE   (DISPLAY_WIDTH * DISPLAY_HEIGHT * DISPLAY_DEPTH)
@@ -43,80 +43,62 @@ typedef struct {
     uint8_t bytes;
 } lcd_init_cmd_t;
 
-void ili9431_blit(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t *bitmap);
-void ili9431_putpixel(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t colour);
-void ili9341_init(spi_device_handle_t *spi);
+void st7735s_blit(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t *bitmap);
+void st7735s_putpixel(spi_device_handle_t spi, uint16_t x1, uint16_t y1, uint16_t colour);
+void st7735s_init(spi_device_handle_t *spi);
 
-#define ILI9341_NOP        0x00
-#define ILI9341_SWRESET    0x01
-#define ILI9341_RDDID      0x04
-#define ILI9341_RDDST      0x09
-
-#define ILI9341_SLPIN      0x10
-#define ILI9341_SLPOUT     0x11
-#define ILI9341_PTLON      0x12
-#define ILI9341_NORON      0x13
-
-#define ILI9341_RDMODE     0x0A
-#define ILI9341_RDMADCTL   0x0B
-#define ILI9341_RDPIXFMT   0x0C
-#define ILI9341_RDIMGFMT   0x0D
-#define ILI9341_RDSELFDIAG 0x0F
-
-#define ILI9341_INVOFF     0x20
-#define ILI9341_INVON      0x21
-#define ILI9341_GAMMASET   0x26
-#define ILI9341_DISPOFF    0x28
-#define ILI9341_DISPON     0x29
-
-#define ILI9341_CASET      0x2A
-#define ILI9341_PASET      0x2B
-#define ILI9341_RAMWR      0x2C
-#define ILI9341_RAMRD      0x2E
-
-#define ILI9341_PTLAR      0x30
-#define ILI9341_MADCTL     0x36
-
-#define ILI9341_MADCTL_MY  0x80 /* Row address order */
-#define ILI9341_MADCTL_MX  0x40 /* Column address order */
-#define ILI9341_MADCTL_MV  0x20 /* Row / columnd exchange */
-#define ILI9341_MADCTL_ML  0x10 /* Vertical refresh order */
-#define ILI9341_MADCTL_RGB 0x00 /* RGB */
-#define ILI9341_MADCTL_BGR 0x08 /* BGR */
-#define ILI9341_MADCTL_MH  0x04 /* Horizontal refresh order */
-
-#define ILI9341_VSCRSADD   0x37
-#define ILI9341_PIXFMT     0x3A
-
-#define ILI9341_FRMCTR1    0xB1
-#define ILI9341_FRMCTR2    0xB2
-#define ILI9341_FRMCTR3    0xB3
-#define ILI9341_INVCTR     0xB4
-#define ILI9341_DFUNCTR    0xB6
-
-#define ILI9341_PWCTR1     0xC0
-#define ILI9341_PWCTR2     0xC1
-#define ILI9341_PWCTR3     0xC2
-#define ILI9341_PWCTR4     0xC3
-#define ILI9341_PWCTR5     0xC4
-#define ILI9341_VMCTR1     0xC5
-#define ILI9341_VMCTR2     0xC7
-
-#define ILI9341_PWCTRB     0xCF
-#define ILI9341_PWCTRA     0xCB
-#define ILI9341_PWONCTR    0xED
-#define ILI9341_DRVTCTRA   0xE8
-
-#define ILI9341_TMCTRA     0xEA
-#define ILI9341_PUMPRTC    0xF7
-
-#define ILI9341_RDID1      0xDA
-#define ILI9341_RDID2      0xDB
-#define ILI9341_RDID3      0xDC
-#define ILI9341_RDID4      0xDD
-
-#define ILI9341_GMCTRP1    0xE0
-#define ILI9341_GMCTRN1    0xE1
-
-#define ILI9341_3GENABLE   0xF2
-#define ILI9341_ENTRYMODE  0xB7
+#define ST7735S_NOP         0x00
+#define ST7735S_SWRESET     0x01 /* Software Reset */
+#define ST7735S_RDDID       0x04 /* Read Display ID */
+#define ST7735S_RDDST       0x09 /* Read Display Status */
+#define ST7735S_RDDPM       0x0a /* Read Display Power Mode */
+#define ST7735S_RDDMADCTL   0x0b /* Read Display MADCTL */
+#define ST7735S_RDDCOLMOD   0x0c /* Read Display Pixel Format */
+#define ST7735S_RDDIM       0x0d /* Read Display Image Mode */
+#define ST7735S_RDDSM       0x0e /* Read Display Signal Mode */
+#define ST7735S_RDDSDR      0x0f /* Read Display Self-Diagnostic Result */
+#define ST7735S_SLPIN       0x10 /* Sleep In */
+#define ST7735S_SLPOUT      0x11 /* Sleep Out */
+#define ST7735S_PTLON       0x12 /* Partial Display Mode On */
+#define ST7735S_NORON       0x13 /* Normal Display Mode On */
+#define ST7735S_INVOFF      0x20 /* Display Inversion Off */
+#define ST7735S_INVON       0x21 /* Display Inversion On */
+#define ST7735S_GAMSET      0x26 /* Gamma Set */
+#define ST7735S_DISPOFF     0x28 /* Display Off */
+#define ST7735S_DISPON      0x29 /* Display On */
+#define ST7735S_CASET       0x2a /* Column Address Set */
+#define ST7735S_RASET       0x2b /* Row Address Set */
+#define ST7735S_RAMWR       0x2c /* Memory Write */
+#define ST7735S_RGBSET      0x2d /* Color Setting 4k, 65k, 262k */
+#define ST7735S_RAMRD       0x2e /* Memory Read */
+#define ST7735S_PTLAR       0x30 /* Partial Area */
+#define ST7735S_SCRLAR      0x33 /* Scroll Area Set */
+#define ST7735S_TEOFF       0x34 /* Tearing Effect Line OFF */
+#define ST7735S_TEON        0x35 /* Tearing Effect Line ON */
+#define ST7735S_MADCTL      0x36 /* Memory Data Access Control */
+#define ST7735S_VSCSAD      0x37 /* Vertical Scroll Start Address of RAM */
+#define ST7735S_IDMOFF      0x38 /* Idle Mode Off */
+#define ST7735S_IDMON       0x39 /* Idle Mode On */
+#define ST7735S_COLMOD      0x3a /* Interface Pixel Format */
+#define ST7735S_RDID1       0xda /* Read ID1 Value */
+#define ST7735S_RDID2       0xdb /* Read ID2 Value */
+#define ST7735S_RDID3       0xdc /* Read ID3 Value */
+#define ST7735S_FRMCTR1     0xb1 /* Frame Rate Control in normal mode, full colors */
+#define ST7735S_FRMCTR2     0xb2 /* Frame Rate Control in idle mode, 8 colors */
+#define ST7735S_FRMCTR3     0xb3 /* Frame Rate Control in partial mode, full colors */
+#define ST7735S_INVCTR      0xb4 /* Display Inversion Control */
+#define ST7735S_PWCTR1      0xc0 /* Power Control 1 */
+#define ST7735S_PWCTR2      0xc1 /* Power Control 2 */
+#define ST7735S_PWCTR3      0xc2 /* Power Control 3 in normal mode, full colors */
+#define ST7735S_PWCTR4      0xc3 /* Power Control 4 in idle mode 8colors */
+#define ST7735S_PWCTR5      0xc4 /* Power Control 5 in partial mode, full colors */
+#define ST7735S_VMCTR1      0xc5 /* VCOM Control 1 */
+#define ST7735S_VMOFCTR     0xc7 /* VCOM Offset Control */
+#define ST7735S_WRID2       0xd1 /* Write ID2 Value */
+#define ST7735S_WRID3       0xd2 /* Write ID3 Value */
+#define ST7735S_NVFCTR1     0xd9 /* NVM Control Status */
+#define ST7735S_NVFCTR2     0xde /* NVM Read Command */
+#define ST7735S_NVFCTR3     0xdf /* NVM Write Command */
+#define ST7735S_GMCTRP1     0xe0 /* Gamma '+'Polarity Correctio Characteristics Setting */
+#define ST7735S_GMCTRN1     0xe1 /* Gamma '-'Polarity Correctio Characteristics Setting */
+#define ST7735S_GCV         0xfc /* Gate Pump Clock Frequency Variable */
